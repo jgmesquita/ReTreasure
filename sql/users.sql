e2d58cbf2634c23e722fcac67b70fd825dfb3b5a
tatianawl <114870093+tatianawl@users.noreply.github.com>
2024-05-19 21:15:52 +0100

    populado

--- sql/users.sql
+++ sql/users.sql
@@ -133,6 +133,7 @@ INSERT INTO users VALUES ('jgmesquita', 'pw', 'Jorge', 'Mesquita', 'adress', 'ci
 INSERT INTO users VALUES ('user1', 'pw', 'Jorge', 'Mesquita', 'adress', 'city', 'country', 'postalCode', 'email', 'phone');
 INSERT INTO users VALUES ('W', '12345678W', 'Tatiana', 'Lin', 'adress', 'city', 'country', 'postalCode', 'email', 'phone');
 INSERT INTO users VALUES ('T', '87654321', 'Tatiana', 'Lin', 'adress', 'city', 'country', 'postalCode', 'email', 'phone');
+INSERT INTO users VALUES ('L', '98989898', 'Tatiana', 'Lin', 'adress', 'city', 'country', 'postalCode', 'email', 'phone');
 
 INSERT INTO adminUser VALUES ('jgmesquita');
 
@@ -248,69 +249,53 @@ INSERT INTO conditions VALUES ('Com Defeito');
 INSERT INTO conditions VALUES ('Novo');
 
 --ITEMS--
+
 -- Electronics
-INSERT INTO items VALUES (1, 'W', 'ELECTRONICS-Mobile Phones', 'Smartphone', 'M', 'black', 900, 'Apple', '12 Pro', 'Novo', '/images/C.png');
+INSERT INTO items VALUES (1, 'W', 'ELECTRONICS-Mobile Phones', 'Smartphone', 'M', 'black', 900, 'Apple', '12 Pro', 'Pouco Usado', '/images/C.png');
 INSERT INTO items VALUES (2, 'W', 'ELECTRONICS-Laptops and Computers', 'Laptop', '15-inch', 'silver', 1000, 'BrandY', 'ModelB', 'Novo', '/images/D.png');
 INSERT INTO items VALUES (3, 'W', 'ELECTRONICS-Cameras', 'DSLR Camera', 'N/A', 'black', 500, 'BrandZ', 'ModelC', 'Novo', '/images/E.png');
 
 -- Fashion
 INSERT INTO items VALUES (4, 'W', 'FASHION-Clothing', 'Dress', 'S', 'blue', 20, 'BrandA', 'ModelD', 'Novo', '/images/dress3.png');
 INSERT INTO items VALUES (5, 'W', 'FASHION-Footwear', 'Sneakers', '37', 'white-beige', 60, 'Nike', 'ModelE', 'Novo', '/images/shoes.png');
-INSERT INTO items VALUES (6, 'W', 'FASHION-Accessories', 'Watch', 'N/A', 'black', 150, 'BrandC', 'ModelF', 'Novo', '/images/B.png');
+INSERT INTO items VALUES (6, 'W', 'FASHION-Accessories', 'Earring', 'N/A', 'gold', 150, 'BrandC', 'ModelF', 'Pouco Usado', '/images/B.png');
 
 -- Home
-INSERT INTO items VALUES (7, 'W', 'HOME-Furniture', 'Sofa', '3-seater', 'grey', 400, 'BrandD', 'ModelG', 'Novo', '/images/B.png');
+INSERT INTO items VALUES (7, 'W', 'HOME-Furniture', 'Sofa', '3-seater', 'beige', 200, 'BrandD', 'ModelG', 'Muito Usado', '/images/sofa.png');
 INSERT INTO items VALUES (8, 'W', 'HOME-Home Decor', 'Wall Art', 'N/A', 'multicolor', 80, 'BrandE', 'ModelH', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (9, 'W', 'HOME-Kitchenware', 'Cookware Set', '10-piece', 'stainless steel', 200, 'BrandF', 'ModelI', 'Novo', '/images/B.png');
+INSERT INTO items VALUES (9, 'W', 'HOME-Kitchenware', 'Cookware Set', '10-piece', 'stainless steel', 100, 'BrandF', 'ModelI', 'Novo', '/images/cook.png');
 
 -- Health
-INSERT INTO items VALUES (10, 'W', 'HEALTH-Skincare', 'Moisturizer', '50ml', 'N/A', 30, 'BrandG', 'ModelJ', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (11, 'W', 'HEALTH-Haircare', 'Shampoo', '250ml', 'N/A', 15, 'BrandH', 'ModelK', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (12, 'W', 'HEALTH-Makeup', 'Lipstick', 'N/A', 'red', 20, 'BrandI', 'ModelL', 'Novo', '/images/A.png');
+INSERT INTO items VALUES (10, 'W', 'HEALTH-Skincare', 'Moisturizer', '50ml', 'N/A', 60, 'Lamer', 'ModelJ', 'Novo', '/images/lamer.png');
+INSERT INTO items VALUES (11, 'W', 'HEALTH-Haircare', 'Shampoo', '250ml', 'N/A', 30, 'Loreal', 'ModelK', 'Novo', '/images/shampoo.png');
+INSERT INTO items VALUES (12, 'W', 'HEALTH-Makeup', 'Lipstick', 'N/A', 'red', 80, 'YSL', 'ModelL', 'Novo', '/images/ysl.png');
 
 -- Sports
-INSERT INTO items VALUES (13, 'W', 'SPORTS-Exercise Equipment', 'Treadmill', 'N/A', 'black', 800, 'BrandJ', 'ModelM', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (14, 'W', 'SPORTS-Outdoor Gear', 'Tent', '4-person', 'green', 100, 'BrandK', 'ModelN', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (15, 'W', 'SPORTS-Sports Apparel', 'Running Shorts', 'M', 'blue', 25, 'BrandL', 'ModelO', 'Novo', '/images/A.png');
+INSERT INTO items VALUES (13, 'W', 'SPORTS-Exercise Equipment', 'Treadmill', 'N/A', 'black', 800, 'BrandJ', 'ModelM', 'Novo', '/images/treadmill.png');
+INSERT INTO items VALUES (14, 'W', 'SPORTS-Outdoor Gear', 'Tent', '4-person', 'green', 100, 'BrandK', 'ModelN', 'Novo', '/images/tent.png');
+INSERT INTO items VALUES (15, 'W', 'SPORTS-Sports Apparel', 'Running Shorts', 'M', 'grey', 25, 'Nike', 'ModelO', 'Muito Usado', '/images/shorts.png');
 
 -- Toys
-INSERT INTO items VALUES (16, 'W', 'TOYS-Action Figures', 'Superhero Figure', 'N/A', 'multicolor', 30, 'BrandM', 'ModelP', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (17, 'W', 'TOYS-Puzzles and Board Games', 'Puzzle', '1000 pieces', 'multicolor', 20, 'BrandN', 'ModelQ', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (18, 'W', 'TOYS-Arts and Crafts', 'Paint Set', 'N/A', 'multicolor', 15, 'BrandO', 'ModelR', 'Novo', '/images/B.png');
+INSERT INTO items VALUES (16, 'W', 'TOYS-Action Figures', 'Superhero Figure', 'N/A', 'multicolor', 20, 'BrandM', 'ModelP', 'Novo', '/images/superhero.png');
+INSERT INTO items VALUES (17, 'W', 'TOYS-Puzzles and Board Games', 'Puzzle', '1000 pieces', 'multicolor', 50, 'BrandN', 'ModelQ', 'Muito Usado', '/images/puzzle.png');
+INSERT INTO items VALUES (18, 'W', 'TOYS-Arts and Crafts', 'Paint Set', 'N/A', 'multicolor', 15, 'BrandO', 'ModelR', 'Novo', '/images/paint.png');
 
 -- Automotive
-INSERT INTO items VALUES (19, 'W', 'AUTOMOTIVE-Car Accessories', 'Car Cover', 'N/A', 'grey', 50, 'BrandP', 'ModelS', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (20, 'W', 'AUTOMOTIVE-Motorcycle Gear', 'Helmet', 'M', 'black', 100, 'BrandQ', 'ModelT', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (21, 'W', 'AUTOMOTIVE-Tools and Equipment', 'Tool Kit', '100-piece', 'N/A', 150, 'BrandR', 'ModelU', 'Novo', '/images/tooBlkit1.png');
+INSERT INTO items VALUES (19, 'W', 'AUTOMOTIVE-Car Accessories', 'Car Cover', 'N/A', 'grey', 30, 'BrandP', 'ModelS', 'Muito Usado', '/images/carcover.png');
+INSERT INTO items VALUES (20, 'W', 'AUTOMOTIVE-Motorcycle Gear', 'Helmet', 'M', 'black', 100, 'BrandQ', 'ModelT', 'Novo', '/images/helmet.png');
+INSERT INTO items VALUES (21, 'W', 'AUTOMOTIVE-Cars', 'Car', 'N/A', 'N/A', 15000, 'BrandR', 'ModelU', 'Muito Usado', '/images/car.png');
 
 -- Books
-INSERT INTO items VALUES (22, 'W', 'BOOKS-Books', 'Novel', 'N/A', 'N/A', 10, 'BrandS', 'ModelV', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (23, 'W', 'BOOKS-Magazines', 'Lifestyle Magazine', 'N/A', 'N/A', 5, 'BrandT', 'ModelW', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (24, 'W', 'BOOKS-Music', 'Music Album', 'N/A', 'N/A', 15, 'BrandU', 'ModelX', 'Novo', '/images/A.png');
-
--- Food
-INSERT INTO items VALUES (25, 'W', 'FOOD-Groceries', 'Cereal', '500g', 'N/A', 5, 'BrandV', 'ModelY', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (26, 'W', 'FOOD-Gourmet Food', 'Truffle Oil', '100ml', 'N/A', 20, 'BrandW', 'ModelZ', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (27, 'W', 'FOOD-Beverages', 'Coffee', '250g', 'N/A', 10, 'BrandX', 'ModelAA', 'Novo', '/images/A.png');
+INSERT INTO items VALUES (22, 'W', 'BOOKS-Books', 'Novel', 'N/A', 'N/A', 10, 'BrandS', 'ModelV', 'Pouco Usado', '/images/book.png');
+INSERT INTO items VALUES (23, 'W', 'BOOKS-Magazines', 'Fashion Magazine', 'N/A', 'N/A', 20, 'Vogue', 'ModelW', 'Novo', '/images/vogue.png');
+INSERT INTO items VALUES (24, 'W', 'BOOKS-Music', 'Music Album', 'N/A', 'N/A', 35, 'BrandU', 'ModelX', 'Pouco Usado', '/images/album.png');
 
 -- Office
-INSERT INTO items VALUES (28, 'W', 'OFFICE-Stationery', 'Notebook', 'A4', 'black', 5, 'BrandY', 'ModelBB', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (29, 'W', 'OFFICE-Office Furniture', 'Desk Chair', 'N/A', 'black', 100, 'BrandZ', 'ModelCC', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (30, 'W', 'OFFICE-Printers and Scanners', 'Printer', 'N/A', 'white', 150, 'BrandAA', 'ModelDD', 'Novo', '/images/B.png');
-
--- Pets
-INSERT INTO items VALUES (31, 'W', 'PETS-Pet Food', 'Dog Food', '10kg', 'N/A', 50, 'BrandBB', 'ModelEE', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (32, 'W', 'PETS-Pet Toys', 'Cat Toy', 'N/A', 'multicolor', 10, 'BrandCC', 'ModelFF', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (33, 'W', 'PETS-Pet Grooming', 'Dog Shampoo', '500ml', 'N/A', 15, 'BrandDD', 'ModelGG', 'Novo', '/images/A.png');
-
--- Baby
-INSERT INTO items VALUES (34, 'W', 'BABY-Baby Gear', 'Stroller', 'N/A', 'black', 200, 'BrandEE', 'ModelHH', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (35, 'W', 'BABY-Nursery Furniture', 'Crib', 'N/A', 'white', 300, 'BrandFF', 'ModelII', 'Novo', '/images/B.png');
-INSERT INTO items VALUES (36, 'W', 'BABY-Baby Clothing', 'Onesie', '6 months', 'blue', 10, 'BrandGG', 'ModelJJ', 'Novo', '/images/A.png');
+INSERT INTO items VALUES (25, 'W', 'OFFICE-Stationery', 'Notebook', 'A3', 'green', 5, 'BrandY', 'ModelBB', 'Novo', '/images/nb.png');
+INSERT INTO items VALUES (26, 'W', 'OFFICE-Office Furniture', 'Desk Chair', 'N/A', 'white', 100, 'BrandZ', 'ModelCC', 'Novo', '/images/deskchair.png');
+INSERT INTO items VALUES (27, 'W', 'OFFICE-Printers and Scanners', 'Printer', 'N/A', 'black', 150, 'BrandAA', 'ModelDD', 'Muito Usado', '/images/printer.png');
 
 -- Jewelry
-INSERT INTO items VALUES (37, 'W', 'JEWELRY-Fine Jewelry', 'Diamond Ring', 'N/A', 'gold', 1000, 'BrandHH', 'ModelKK', 'Novo', '/images/A.png');
-INSERT INTO items VALUES (38, 'W', 'JEWELRY-Fashion Jewelry', 'Necklace', 'N/A', 'silver', 50, 'BrandII', 'ModelLL', 'Novo', '/images/A.png');
 INSERT INTO items VALUES (39, 'W', 'JEWELRY-Watches', 'Watch', 'N/A', 'black', 50, 'BrandII', 'ModelLL', 'Novo', '/images/G.png');
 INSERT INTO items VALUES (40, 'W', 'JEWELRY-Watches', 'Watch', 'N/A', 'silver', 1000, 'Rolex', 'ModelLL', 'Novo', '/images/H.png');
 INSERT INTO items VALUES (41, 'W', 'JEWELRY-Watches', 'Watch', 'N/A', 'gold', 50, 'BrandII', 'ModelLL', 'Novo', '/images/I.png');
@@ -321,4 +306,10 @@ INSERT INTO items VALUES (43, 'T', 'ELECTRONICS-Cameras', 'Vintage Camera', 'N/A
 INSERT INTO items VALUES (44, 'T', 'FASHION-Clothing', 'Dress', 'L', 'white', 40, 'ZARA', 'ModelD', 'Novo', '/images/dress.png');
 INSERT INTO items VALUES (45, 'T', 'HOME-Home Decor', 'Wall Art', 'N/A', 'multicolor', 10000, 'BrandE', 'ModelH', 'Novo', '/images/art.png');
 INSERT INTO items VALUES (46, 'W', 'FASHION-Clothing', 'Dress', 'S', 'blue', 20, 'BrandA', 'ModelD', 'Pouco Usado', '/images/dress2.png');
+INSERT INTO items VALUES (47, 'L', 'HEALTH-Makeup', 'Lipstick', 'N/A', 'red', 60, 'YSL', 'ModelL', 'Novo', '/images/ysl2.png');
+
+INSERT INTO items VALUES (48, 'L', 'BOOKS-Books', 'Novel', 'N/A', 'N/A', 20, 'BrandS', 'ModelV', 'Pouco Usado', '/images/book2.png');
+INSERT INTO items VALUES (49, 'T', 'BOOKS-Magazines', 'Fashion Magazine', 'N/A', 'N/A', 40, 'Vogue', 'ModelW', 'Novo', '/images/vogue2.png');
+INSERT INTO items VALUES (50, 'W', 'BOOKS-Music', 'Music Album', 'N/A', 'N/A', 25, 'BrandU', 'ModelX', 'Pouco Usado', '/images/album2.png');
+
 INSERT INTO comment VALUES(1,1, 'user1', 'What is the price?');
